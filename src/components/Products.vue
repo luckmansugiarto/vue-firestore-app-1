2f808dcdc6c0b1a66453b2b6a518a84cf4424d87
Luckman Sugiarto (QuSol) <luckman.sugiarto@qusol.com.au>
2018-10-15 13:29:29 +1100

    Initial commit

---
+++ src/components/Products.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ products }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    data () {
+        return {
+            products: 'Testing'
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+// Initialize Firebase
+const config = {
+    apiKey: 'AIzaSyDwc833C6fQaRfAr42rhzvLxGB3lI99Voc',
+    authDomain: 'app-2-352ff.firebaseapp.com',
+    databaseURL: 'https://app-2-352ff.firebaseio.com',
+    projectId: 'app-2-352ff',
+    storageBucket: 'app-2-352ff.appspot.com',
+    messagingSenderId: '21039500689'
+}
+const firebase.initializeApp(config)
+
+export default firebaseapp.firestore
